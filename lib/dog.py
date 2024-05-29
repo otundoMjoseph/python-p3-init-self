235fc46034d971f9a92559a6317c5596a833f678
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-05-29 16:56:21 +0300

    test passed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print("Woof!")
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+fido = Dog("Fido")
+snoopy = Dog("Snoopy")
+
+fido.get_adopted("Sophie")
+
+print(fido.owner)
+  
